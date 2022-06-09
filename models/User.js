dcb890a2323e482cd176912b58ee89b8ae04b78e
whrksp126 <whrksp126@naver.com>
2022-06-09 23:12:14 +0900

    회원가입 api

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true},
   // unique: true는 동일한 username이 있으면 error 출력함
   username: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
+  hashedPassword: {type: String, required: true},
 },{timestamps: true}
 )
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,29 @@
 const { Router } = require('express');
 const userRouter = Router();
 const User = require('../models/User')
+// npm i bcryptjs ( 암호화 라이브러리 )
+const { hash } = require("bcryptjs")
 userRouter.post('/register', async (req, res) => {
-  await new User(req.body).save()
-  res.json({  message: "user registered" })
+  try{
+    if(req.body.password.length < 6) 
+      throw new Error("비밀번호를 6자 이상으로 해주세요.")
+    if(req.body.username.length < 3) 
+      throw new Error("username은 3자 이상으로 해주세요.")
+    // hash 함수를 이용해서 req.body.password를 암호화 해준다. 다음 인자(10)는 암호화 등급으로 숫자가 높을 수록 높은 보안이다(단 높으면 느려짐). 
+    const hashedPassword = await hash(req.body.password, 10)
+    
+    await new User({
+      name: req.body.name,
+      username: req.body.username,
+      hashedPassword
+    }).save()
+    res.json({  message: "user registered" })
+  }catch(err){
+    res.status(400).json({  message: err.message })
+  }
+
 })
 
 module.exports = { userRouter };
+
 
